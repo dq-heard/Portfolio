c3d9e3f9b3e45b4cfe3d278338240bc8178d611b
DeQuentin Heard <dq.heard@yahoo.com>
2025-03-20 03:56:56 -0500

    CodeBlock Animation Fix

--- src/graphics/CodeBlock.tsx
+++ src/graphics/CodeBlock.tsx
@@ -5,7 +5,7 @@ const CodeBlock = () => {
 
   useEffect(() => {
     const delay =
-      (18 + 26 + 21 + 20 + 24 + 15 + 18 + 17 + 18 + 5 + 3) * 150 + 3000; // Total time in ms
+      (19 + 25 + 20 + 20 + 25 + 14 + 16 + 15 + 17 + 5 + 3) * 150 + 3000; // Total time in ms
 
     const intervalID = setInterval(() => {
       setAnimationState("fade-out");
@@ -28,52 +28,54 @@ const CodeBlock = () => {
         transition: "opacity 1s ease-in-out",
       }}
     >
-      <span className="line01">
+      <span id="line01" className="lines">
         <span style={{ color: "rgb(121, 40, 161)" }}>const </span>
         <span>developer = {"{"}</span>
       </span>
-      <span className="line02">
+      <span id="line02" className="lines">
         <span style={{ paddingLeft: "15px" }} />
         <span>firstName:</span>
         <span style={{ color: "rgb(0, 128, 0)" }}>"DeQuentin"</span>
         <span>,</span>
       </span>
-      <span className="line03">
+      <span id="line03" className="lines">
         <span style={{ paddingLeft: "15px" }} />
         <span>lastName:</span>
         <span style={{ color: "rgb(0, 128, 0)" }}>"Heard"</span>
         <span>,</span>
       </span>
-      <span className="line04">
+      <span id="line04" className="lines">
         <span style={{ paddingLeft: "15px" }} />
         <span>type:</span>
         <span style={{ color: "rgb(0, 128, 0)" }}>"Front End"</span>
         <span>,</span>
       </span>
-      <span className="line05">
+      <span id="line05" className="lines">
         <span style={{ paddingLeft: "15px" }} />
         <span>hobby:repeat = () =&gt; {"{"}</span>
       </span>
-      <span className="line06">
+      <span id="line06" className="lines">
         <span style={{ paddingLeft: "60px" }} />
         <span style={{ color: "var(--text-color-light)" }}>//eat();</span>
       </span>
-      <span className="line07">
+      <span id="line07" className="lines">
         <span style={{ paddingLeft: "60px" }} />
         <span style={{ color: "var(--text-color-light)" }}>//sleep();</span>
       </span>
-      <span className="line08">
+      <span id="line08" className="lines">
         <span style={{ paddingLeft: "60px" }} />
         <span style={{ color: "var(--text-color-light)" }}>//code();</span>
       </span>
-      <span className="line09">
+      <span id="line09" className="lines">
         <span style={{ paddingLeft: "60px" }} />
         <span style={{ color: "var(--text-color-light)" }}>//repeat();</span>
       </span>
-      <span className="line10" style={{ paddingLeft: "15px" }}>
+      <span id="line10" className="lines" style={{ paddingLeft: "15px" }}>
         {"}"}
       </span>
-      <span className="line11">{"};"}</span>
+      <span id="line11" className="lines">
+        {"};"}
+      </span>
     </code>
   );
 };
